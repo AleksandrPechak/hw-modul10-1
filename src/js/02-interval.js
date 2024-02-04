bb74eac312cfdd574fcbac7b54566d5f0ca43e6a
AleksandrPechak <144905025+AleksandrPechak@users.noreply.github.com>
2024-02-04 18:31:55 +0200

    update

---
+++ src/js/02-interval.js
@@ -0,0 +1,18 @@
+/*
+ * Метод setInterval(callback, delay, args)
+ */
+
+// let counter = 0;
+// const id1 = setInterval(() => {
+//   console.log('Tick', counter++);
+// }, 1000);
+
+// const id2 = setInterval(() => {
+//   console.log('---------');
+// }, 5000);
+
+/*
+ * Очищення інтервалу за допомогою clearInterval(intervalId)
+ */
+
+// clearInterval(id1);
---
+++ src/js/03-notification.js
@@ -0,0 +1,37 @@
+/**
+ * - Показуємо та ховаємо, додаючи/видаляючи клас is-visible
+ * - Ховаємо через певний час
+ * - Ховаємо при кліці
+ * - Не забуваємо чистити таймер
+ */
+
+const NOTIFICATION_DELAY = 3000;
+const notification = document.querySelector('.js-alert');
+let timeoutId = null;
+
+// const intervalId = setInterval(() => {
+//   showNotification();
+//   timeoutId = setTimeout(hideNotification, 4000);
+// }, 5000);
+
+// notification.addEventListener('click', () => {
+//   hideNotification();
+//   clearTimeout(timeoutId);
+// });
+
+// setTimeout(() => {
+//   clearInterval(intervalId);
+// }, 20000);
+
+/* 
+Функції
+*/
+
+function showNotification() {
+  console.log('Show');
+  notification.classList.add('is-visible');
+}
+function hideNotification() {
+  console.log('Hide');
+  notification.classList.remove('is-visible');
+}
